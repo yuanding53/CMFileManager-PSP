a87ab26319555334d443fa8f6fed1878b45a4e6b
yuanding53 <32296267+yuanding53@users.noreply.github.com>
2025-09-22 17:39:01 +0800

    font use gb3s1518.bwfon

--- app/source/main.cpp
+++ app/source/main.cpp
@@ -38,7 +38,7 @@ namespace Services {
             return ret;
         }
         
-        font = intraFontLoad("flash0:/font/ltn8.pgf", INTRAFONT_CACHE_ALL);
+        font = intraFontLoad("flash0:/font/gb3s1518.bwfon", INTRAFONT_CACHE_ALL);
         G2D::FontSetStyle(1.f, WHITE, INTRAFONT_ALIGN_LEFT);
         
         // Font size cache
