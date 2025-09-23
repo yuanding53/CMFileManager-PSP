1b1b11b2b01c3655b5604c23e55e924d3a19e580
dingyuan <dingyuan@tencent.com>
2025-09-23 14:36:35 +0800

    test

--- app/source/main.cpp
+++ app/source/main.cpp
@@ -39,7 +39,7 @@ namespace Services {
         }
         
         font = intraFontLoad("flash0:/font/gb3s1518.bwfon", INTRAFONT_CACHE_ALL);
-        intraFontSetEncoding(font, INTRAFONT_STRING_UTF8);
+        intraFontSetEncoding(font, INTRAFONT_STRING_GBK);
         G2D::FontSetStyle(1.f, WHITE, INTRAFONT_ALIGN_LEFT);
 
         // Font size cache
